225803f3c59a3f4d5fe8aae95f3d6ca400bc1824
Yiseull <omjl5123@catholic.ac.kr>
2022-12-05 00:57:56 +0900

    First solution

--- 3986_좋은 단어.py
+++ 3986_좋은 단어.py
@@ -1,15 +1,12 @@
-# 알고리즘 분류:
-
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 words = [input().strip() for _ in range(n)]
 
-stack = []
 cnt = 0
 for word in words:
-    stack.clear()
+    stack = []
     for c in word:
         if len(stack) != 0 and c == stack[-1]:
             stack.pop()
