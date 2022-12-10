57934052c5c2a6662552a4b27e43f0d95babf6c7
Yiseull <omjl5123@catholic.ac.kr>
2022-12-10 11:53:02 +0900

    First solution

---
+++ 4659_비밀번호 발음하기.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+consonant = set(['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'])
+vowel = set(['a', 'e', 'i', 'o', 'u'])
+while 1:
+    password = input().strip()
+    if password == 'end':
+        break
+
+    condition1, condition2_con, condition2_vow, condition3 = False, 0, 0, False
+    for i, char in enumerate(password):
+        if char in ['a', 'e', 'i', 'o', 'u']:
+            condition1 = True
+
+        if char in consonant:
+            condition2_vow = 0
+            condition2_con += 1
+            if condition2_con == 3:
+                break
+
+        if char in vowel:
+            condition2_con = 0
+            condition2_vow += 1
+            if condition2_vow == 3:
+                break
+
+        if i > 0 and password[i - 1] == char:
+            if char == 'e' or char == 'o':
+                continue
+            condition3 = True
+            break
+
+    if not condition1 or condition2_con > 2 or condition2_vow > 2 or condition3:
+        print('<' + password + '> is not acceptable.')
+        continue
+
+    print('<' + password + '> is acceptable.')
