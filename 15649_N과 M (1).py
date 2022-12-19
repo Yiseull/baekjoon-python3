303f7fec789d766406af3a15289969a4d9dcc61d
Yiseull <omjl5123@catholic.ac.kr>
2022-12-19 14:25:07 +0900

    Second solution

--- 15649_N과 M (1).py
+++ 15649_N과 M (1).py
@@ -1,19 +1,8 @@
 import sys
+from itertools import permutations
 input = sys.stdin.readline
 
-
-def printSeq(seq):
-    if len(seq) == m:
-        result = ''
-        for i in seq:
-            result += i + ' '
-        print(result)
-        return
-
-    for i in range(1, n + 1):
-        if str(i) not in seq:
-            printSeq(seq + str(i))
-
-
 n, m = map(int, input().split())
-printSeq('')
+nums = [str(i) for i in range(1, n + 1)]
+pers = list(permutations(nums, m))
+print('\n'.join(' '.join(per) for per in pers))
