86c50b7ac30748d2036b490b4ff138853058ddbe
Yiseull <omjl5123@catholic.ac.kr>
2022-12-21 17:10:54 +0900

    First solution

---
+++ 14620_꽃길.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+prices = [list(map(int, input().split())) for _ in range(n)]
+mapp = [[0] * n for _ in range(n)]
+result = 20000
+
+
+def count_price(price, cnt):
+    if cnt == 3:
+        global result
+        result = min(result, price)
+        return
+
+    for i in range(1, n - 1):
+        for j in range(1, n - 1):
+            if mapp[i][j] != 1 and mapp[i - 1][j] != 1 and mapp[i + 1][j] != 1 and mapp[i][j - 1] != 1 and mapp[i][j + 1] != 1:
+                mapp[i][j], mapp[i - 1][j], mapp[i + 1][j], mapp[i][j - 1], mapp[i][j + 1] = 1, 1, 1, 1, 1
+                price2 = price
+                price2 += prices[i][j] + prices[i - 1][j] + prices[i + 1][j] + prices[i][j - 1] + prices[i][j + 1]
+                count_price(price2, cnt + 1)
+                mapp[i][j], mapp[i - 1][j], mapp[i + 1][j], mapp[i][j - 1], mapp[i][j + 1] = 0, 0, 0, 0, 0
+
+
+count_price(0, 0)
+print(result)
