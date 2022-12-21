c5436e71914987bf18022c3e6441b84010b494a7
Yiseull <omjl5123@catholic.ac.kr>
2022-12-21 16:11:40 +0900

    Second solution

--- 1189_컴백홈.py
+++ 1189_컴백홈.py
@@ -1,29 +1,28 @@
 import sys
-from copy import deepcopy
 input = sys.stdin.readline
 
 r, c, k = map(int, input().split())
 mapp = [input().strip() for _ in range(r)]
+visited = [[False for _ in range(c)] for _ in range(r)]
+visited[r - 1][0] = True
 result = 0
 
 
-def dfs(x, y, cnt, visited):
+def dfs(x, y, cnt):
     if x == c - 1 and y == 0:
         if cnt == k:
             global result
             result += 1
         return
 
-    visited[y][x] = True
-    new_visited = deepcopy(visited)
     for dx, dy in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
         next_x = x + dx
         next_y = y + dy
-        if 0 <= next_x < c and 0 <= next_y < r and mapp[next_y][next_x] == '.' and not new_visited[next_y][next_x]:
-            new_visited[next_y][next_x] = True
-            dfs(next_x, next_y, cnt + 1, new_visited)
-            new_visited[next_y][next_x] = False
+        if 0 <= next_x < c and 0 <= next_y < r and mapp[next_y][next_x] == '.' and not visited[next_y][next_x]:
+            visited[next_y][next_x] = True
+            dfs(next_x, next_y, cnt + 1)
+            visited[next_y][next_x] = False
 
 
-dfs(0, r - 1, 1, [[False for _ in range(c)] for _ in range(r)])
+dfs(0, r - 1, 1)
 print(result)
