88cf9c4317e491162ad2ea6fa29b74db5e928895
Yiseull <omjl5123@catholic.ac.kr>
2022-12-24 13:06:12 +0900

    Second solution

--- 15686_치킨 배달.py
+++ 15686_치킨 배달.py
@@ -1,5 +1,4 @@
 import sys
-from copy import deepcopy
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -20,13 +19,14 @@ def chicken_street(cnt, distance, idx):
     if cnt == m:
         return
 
-    for i in range(idx, len(chicken)):
-        new_distance = deepcopy(distance)
-        for j in range(len(house)):
+    for i in range(idx, chicken_len):
+        new_distance = distance[:]
+        for j in range(house_len):
             new_distance[j] = min(new_distance[j], abs(chicken[i][0] - house[j][0]) + abs(chicken[i][1] - house[j][1]))
         result = min(result, sum(new_distance))
         chicken_street(cnt + 1, new_distance, i + 1)
 
 
-chicken_street(0, [100] * len(house), 0)
+house_len, chicken_len = len(house), len(chicken)
+chicken_street(0, [100] * house_len, 0)
 print(result)
