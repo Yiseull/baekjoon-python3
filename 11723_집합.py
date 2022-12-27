4858ce802cf4d7d03c08b98d6c69c5201e660dcb
Yiseull <omjl5123@catholic.ac.kr>
2022-12-28 00:05:36 +0900

    Second solution

--- 11723_집합.py
+++ 11723_집합.py
@@ -3,22 +3,20 @@ input = sys.stdin.readline
 
 s = set()
 for _ in range(int(input())):
-    oper = list(input().split())
+    oper = input().split()
     if oper[0] == 'add':
         s.add(oper[1])
     elif oper[0] == 'remove':
-        x = oper[1]
-        if x in s:
-            s.remove(x)
+        if oper[1] in s:
+            s.remove(oper[1])
     elif oper[0] == 'check':
         print(1 if oper[1] in s else 0)
     elif oper[0] == 'toggle':
-        x = oper[1]
-        if x in s:
-            s.remove(x)
+        if oper[1] in s:
+            s.remove(oper[1])
         else:
-            s.add(x)
+            s.add(oper[1])
     elif oper[0] == 'all':
         s = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'}
-    elif oper[0] == 'empty':
+    else:
         s.clear()
