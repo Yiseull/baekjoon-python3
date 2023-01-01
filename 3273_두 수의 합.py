086e570d74b7014dd999f9a4a37e62015142fd69
Yiseull <omjl5123@catholic.ac.kr>
2023-01-02 04:30:49 +0900

    Second solution

--- 3273_두 수의 합.py
+++ 3273_두 수의 합.py
@@ -5,14 +5,10 @@ n = int(input())
 a = list(map(int, input().split()))
 x = int(input())
 a.sort()
-left, right, cnt = 0, n - 1, 0
-while left < right:
-    sum = a[left] + a[right]
-    if sum < x:
-        left += 1
-    else:
-        if sum == x:
-            cnt += 1
-        right -= 1
+a_set = set(a)
+cnt = 0
+for i in a:
+    if x - i in a_set:
+        cnt += 1
 
-print(cnt)
+print(cnt // 2)
