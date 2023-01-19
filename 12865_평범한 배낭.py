d969d57aa6efd590ae1bae7e7f713355e4282d94
Yiseull <omjl5123@catholic.ac.kr>
2023-01-19 22:16:50 +0900

    First solution

--- 12865_평범한 배낭.py
+++ 12865_평범한 배낭.py
@@ -2,12 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-wv = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * (k + 1) for _ in range(n)]
-for i in range(n):
-    for w in range(1, k + 1):
-        if wv[i][0] > w:
-            dp[i][w] = dp[i - 1][w]
+w, v = [0] * (n + 1), [0] * (n + 1)
+for i in range(1, n + 1):
+    w[i], v[i] = map(int, input().split())
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        if w[i] > j:
+            dp[i][j] = dp[i - 1][j]
         else:
-            dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - wv[i][0]] + wv[i][1])
-print(dp[n - 1][w])
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
+
+print(dp[n][k])
