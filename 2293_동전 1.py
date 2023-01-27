0dadc7cd1c1f8d036ba409038918d3cad046db5f
Yiseull <omjl5123@catholic.ac.kr>
2023-01-27 21:36:43 +0900

    Third solution

--- 2293_동전 1.py
+++ 2293_동전 1.py
@@ -5,11 +5,11 @@ n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 
 dp = [0] * (k + 1)
+dp[0] = 1
 for coin in coins:
     if coin > k:
         continue
-    dp[coin] += 1
-    for i in range(coin + 1, k + 1):
+    for i in range(coin, k + 1):
         dp[i] += dp[i - coin]
 
 print(dp[k])
