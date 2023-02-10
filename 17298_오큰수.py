10a167fa1323abf74e9d34ba96409cc177f7d231
Yiseull <omjl5123@catholic.ac.kr>
2023-02-11 01:05:26 +0900

    Second solution

--- 17298_오큰수.py
+++ 17298_오큰수.py
@@ -3,14 +3,13 @@ input = sys.stdin.readline
 
 n = int(input())
 A = list(map(int, input().split()))
-NGE = [-1 for _ in range(n)]
-stack = []
+NGE = [-1] * n
+stack = [0]
 for i in range(n - 1):
     if A[i] < A[i + 1]:
         NGE[i] = A[i + 1]
         while stack and A[stack[-1]] < A[i + 1]:
-            NGE[stack[-1]] = A[i + 1]
-            stack.pop()
+            NGE[stack.pop()] = A[i + 1]
     else:
         stack.append(i)
 
