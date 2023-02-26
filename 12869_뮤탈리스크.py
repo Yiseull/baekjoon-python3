a92a9b48f969a7622cb9838ca2192daa0106e4bb
Yiseull <omjl5123@catholic.ac.kr>
2023-02-27 01:57:45 +0900

    Second solution

--- 12869_뮤탈리스크.py
+++ 12869_뮤탈리스크.py
@@ -7,18 +7,16 @@ def attack(scv1, scv2, scv3, cnt):
     if scv1 < 0: scv1 = 0
     if scv2 < 0: scv2 = 0
     if scv3 < 0: scv3 = 0
+
     if dp[scv1][scv2][scv3] <= cnt:
         return
     dp[scv1][scv2][scv3] = cnt
+
     if scv1 == 0 and scv2 == 0 and scv3 == 0:
-        dp[0][0][0] = min(dp[0][0][0], cnt)
         return
-    attack(scv1 - 9, scv2 - 3, scv3 - 1, cnt + 1)
-    attack(scv1 - 9, scv2 - 1, scv3 - 3, cnt + 1)
-    attack(scv1 - 3, scv2 - 9, scv3 - 1, cnt + 1)
-    attack(scv1 - 3, scv2 - 1, scv3 - 9, cnt + 1)
-    attack(scv1 - 1, scv2 - 9, scv3 - 3, cnt + 1)
-    attack(scv1 - 1, scv2 - 3, scv3 - 9, cnt + 1)
+
+    for a, b, c in [[9, 3, 1], [9, 1, 3], [3, 9, 1], [3, 1, 9], [1, 9, 3], [1, 3, 9]]:
+        attack(scv1 - a, scv2 - b, scv3 - c, cnt + 1)
 
 
 n = int(input())
@@ -26,9 +24,8 @@ scv = list(map(int, input().split()))
 dp = [[[14 for _ in range(61)] for _ in range(61)] for _ in range(61)]
 if n == 1:
     print(ceil(scv[-1] / 9))
-elif n == 2:
-    attack(scv[0], scv[1], 0, 0)
-    print(dp[0][0][0])
 else:
+    while len(scv) < 3:
+        scv.append(0)
     attack(scv[0], scv[1], scv[2], 0)
     print(dp[0][0][0])
