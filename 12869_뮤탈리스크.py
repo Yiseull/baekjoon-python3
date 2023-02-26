40c554b76f0e4e9f543fe8bf4d1ce7e94786ff23
Yiseull <omjl5123@catholic.ac.kr>
2023-02-27 01:37:32 +0900

    First solution

--- 12869_뮤탈리스크.py
+++ 12869_뮤탈리스크.py
@@ -7,15 +7,12 @@ def attack(scv1, scv2, scv3, cnt):
     if scv1 < 0: scv1 = 0
     if scv2 < 0: scv2 = 0
     if scv3 < 0: scv3 = 0
-
     if dp[scv1][scv2][scv3] <= cnt:
         return
     dp[scv1][scv2][scv3] = cnt
-
     if scv1 == 0 and scv2 == 0 and scv3 == 0:
         dp[0][0][0] = min(dp[0][0][0], cnt)
         return
-
     attack(scv1 - 9, scv2 - 3, scv3 - 1, cnt + 1)
     attack(scv1 - 9, scv2 - 1, scv3 - 3, cnt + 1)
     attack(scv1 - 3, scv2 - 9, scv3 - 1, cnt + 1)
@@ -26,7 +23,7 @@ def attack(scv1, scv2, scv3, cnt):
 
 n = int(input())
 scv = list(map(int, input().split()))
-dp = [[[20 for _ in range(61)] for _ in range(61)] for _ in range(61)]
+dp = [[[14 for _ in range(61)] for _ in range(61)] for _ in range(61)]
 if n == 1:
     print(ceil(scv[-1] / 9))
 elif n == 2:
