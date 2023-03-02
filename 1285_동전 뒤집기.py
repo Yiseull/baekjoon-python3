d53db21c7917577d1f14354a5dc1cd388a36c276
Yiseull <omjl5123@catholic.ac.kr>
2023-03-02 22:09:05 +0900

    First solution

---
+++ 1285_동전 뒤집기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+coins = [list(input().strip()) for _ in range(n)]
+result = n * n
+
+for bit in range(1 << n):
+    tmp = [coins[i][:] for i in range(n)]
+    for i in range(n):
+        if bit & (1 << i):
+            for j in range(n):
+                if tmp[i][j] == 'T':
+                    tmp[i][j] = 'H'
+                else:
+                    tmp[i][j] = 'T'
+    total = 0
+    for i in range(n):
+        cnt = 0
+        for j in range(n):
+            if tmp[j][i] == 'T':
+                cnt += 1
+        total += min(cnt, n - cnt)
+    result = min(result, total)
+    if result == 2:
+        break
+
+print(result)
