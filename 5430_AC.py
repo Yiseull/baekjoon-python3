586fddb49d0e808bfc3f0344cea3b24182bcf49b
Yiseull <omjl5123@catholic.ac.kr>
2023-03-06 13:32:46 +0900

    Second solution

--- 5430_AC.py
+++ 5430_AC.py
@@ -1,53 +1,34 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
-    p = input().strip()
-    while 'PP' in p:
-        p = p.replace('PP', '')
+    p = [*map(len, input().strip().replace('RR', '').split('R'))]
     n = int(input())
     x = input().strip()
-    if n:
-        x = deque(map(int, x[1:-1].split(',')))
-    else:
-        if 'D' in p:
-            print('error')
-            continue
-        else:
-            print('[]')
-            continue
-
-    result = 1
-    start, end = 0, n - 1
-    for i, f in enumerate(p):
-        if f == 'R':
-            start, end = end, start
-        else:
-            if start < end:
-                start += 1
-            elif start > end:
-                start -= 1
-            else:
-                if 'D' in p[i + 1:]:
-                    result = -1
-                    break
-                else:
-                    result = 0
-                    break
-
-    if result == -1:
+    if sum(p) > n:
         print('error')
-    elif result == 0:
-        print('[]')
+        continue
+    if n:
+        x = list(map(int, x[1:-1].split(',')))
     else:
-        if x:
-            ans = '['
-            if start <= end:
-                ans += ','.join(map(str, [x[i] for i in range(start, end + 1)]))
-            else:
-                ans += ','.join(map(str, [x[i] for i in range(start, end - 1, -1)]))
-            print(ans + ']')
+        print('[]')
+        continue
+
+    error = 1
+    start, end = 0, n
+    for i, f in enumerate(p):
+        if i % 2 == 0:
+            start += f
         else:
-            print(x)
+            end -= f
+
+    if error == -1:
+        print('error')
+    else:
+        ans = '['
+        if len(p) % 2 == 0:
+            ans += ','.join(map(str, [x[i] for i in range(end - 1, start - 1, -1)]))
+        else:
+            ans += ','.join(map(str, [x[i] for i in range(start, end)]))
+        print(ans + ']')
