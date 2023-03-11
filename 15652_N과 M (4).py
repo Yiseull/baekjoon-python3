f84c2c98611b070725c87f5d4321a26ff6db025d
Yiseull <omjl5123@catholic.ac.kr>
2023-03-11 13:57:15 +0900

    First solution

--- 15652_N과 M (4).py
+++ 15652_N과 M (4).py
@@ -2,12 +2,12 @@ import sys
 input = sys.stdin.readline
 
 
-def print_per(num, s):
+def print_per(number, s):
     if len(s) == m * 2:
         print(s)
         return
 
-    for i in range(num, n + 1):
+    for i in range(number, n + 1):
         print_per(i, s + str(i) + ' ')
 
 
