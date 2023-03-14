83c0dfe6927d6f8471dcbbd474de5b932b8cab60
Yiseull <omjl5123@catholic.ac.kr>
2023-03-14 23:52:47 +0900

    Second solution

--- 15656_N과 M (7).py
+++ 15656_N과 M (7).py
@@ -1,17 +1,8 @@
 import sys
+from itertools import product
 input = sys.stdin.readline
-
-
-def print_per(index, s, cnt):
-    if cnt == m:
-        print(s)
-        return
-
-    for i in range(n):
-        print_per(i, s + str(nums[i]) + ' ', cnt + 1)
-
 
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 nums.sort()
-print_per(0, '', 0)
+print('\n'.join(list(map(' '.join, product(map(str, nums), repeat=m)))))
