8ba9f999d30be7deb9c314f201cd4a5d7aa44e0b
Yiseull <omjl5123@catholic.ac.kr>
2023-03-18 00:49:09 +0900

    Second solution

--- 16987_계란으로 계란치기.py
+++ 16987_계란으로 계란치기.py
@@ -2,39 +2,36 @@ import sys
 input = sys.stdin.readline
 
 
-def count_broken():
-    cnt = 0
-    for egg in eggs:
-        if egg[0] <= 0:
-            cnt += 1
-    return cnt
-
-
-def dfs(index):
+def dfs(index, broken_cnt):
     if index == n:
         global answer
-        answer = max(answer, count_broken())
+        answer = max(answer, broken_cnt)
         return
 
-    # 손에 든 계란이 깨져있다면, 오른쪽 계란으로 넘어간다.
+    # 손에 든 계란이 깨졌다면 다음 계란으로 넘어 간다.
     if eggs[index][0] <= 0:
-        dfs(index + 1)
+        dfs(index + 1, broken_cnt)
     else:
         all_broken = True
         for i, egg in enumerate(eggs):
-            # 손에 든 계란이거나 이미 깨진 계란은 치지 않고 넘어간다.
+            # 손에 든 계란 or 이미 깨진 계란 이라면 치지 않고 넘어 간다.
             if i == index or egg[0] <= 0:
                 continue
             all_broken = False
             eggs[i][0] -= eggs[index][1]
             eggs[index][0] -= eggs[i][1]
-            dfs(index + 1)
+            tmp = broken_cnt
+            if eggs[i][0] <= 0:
+                tmp += 1
+            if eggs[index][0] <= 0:
+                tmp += 1
+            dfs(index + 1, tmp)
             eggs[i][0] += eggs[index][1]
             eggs[index][0] += eggs[i][1]
 
-        # 모든 계란이 깨져있다면, dfs를 빠져나온다.
+        # 손에 든 계란을 제외한 모든 계란이 깨져 있는 경우
         if all_broken:
-            dfs(n)
+            answer = max(answer, n - 1)
 
 
 n = int(input())
@@ -43,5 +40,5 @@ answer = 0
 if n == 1:
     print(0)
 else:
-    dfs(0)
+    dfs(0, 0)
     print(answer)
