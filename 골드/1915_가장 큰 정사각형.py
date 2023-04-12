e74fb17492b56accdbdfc90a47c746b2bfc5b023
Yiseull <omjl5123@catholic.ac.kr>
2023-04-12 09:54:55 +0900

    Second solution

--- 골드/1915_가장 큰 정사각형.py
+++ 골드/1915_가장 큰 정사각형.py
@@ -2,34 +2,18 @@ import sys
 input = sys.stdin.readline
 
 
-def square(x, y, array, dp) -> int:
-    length = dp[x - 1][y - 1]
-    for i in range(1, length + 1):
-        if array[x - i][y] != '1' or array[x][y - i] != '1':
-            return i
-
-    return length + 1
-
-
 def main() -> None:
     n, m = map(int, input().split())
     array = [input().strip() for _ in range(n)]
-    dp = [[-1] * m for _ in range(n)]
-    max_length = 0
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-    for i in range(n):
-        for j in range(m):
-            if i == 0 or j == 0:
-                dp[i][j] = int(array[i][j])
-            elif array[i][j] == '1':
-                if array[i - 1][j - 1] == '1':
-                    dp[i][j] = square(i, j, array, dp)
-                else:
-                    dp[i][j] = 1
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if array[i - 1][j - 1] == '1':
+                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
 
-            max_length = max(max_length, dp[i][j])
-
-    print(max_length * max_length)
+    max_length = max(map(max, dp))
+    print(max_length ** 2)
 
 
 main()
