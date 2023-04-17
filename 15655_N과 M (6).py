6e18fe2a324cbfa2cc0ecbf93dcbbf564130dce9
Yiseull <omjl5123@catholic.ac.kr>
2023-04-18 06:12:07 +0900

    중복 문제 제거

--- 15655_N과 M (6).py
+++
@@ -1,9 +0,0 @@
-import sys
-from itertools import combinations
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-nums = list(map(int, input().split()))
-nums.sort()
-pers = list(combinations(nums, m))
-print('\n'.join(' '.join(map(str, per)) for per in pers))
