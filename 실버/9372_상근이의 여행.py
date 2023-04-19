ef9270df1088a85b3f455bc5cedb9b8d5f3133dd
Yiseull <omjl5123@catholic.ac.kr>
2023-04-19 16:07:08 +0900

    Second solution: 크루스칼 알고리즘
    크루스칼의 원리를 이용해서 정답 도출
    edge = node - 1

--- 실버/9372_상근이의 여행.py
+++ 실버/9372_상근이의 여행.py
@@ -2,34 +2,11 @@ import sys
 input = sys.stdin.readline
 
 
-def find(parent, x) -> int:
-    if parent[x] == x:
-        return x
-
-    return find(parent, parent[x])
-
-
-def union(parent, a, b) -> None:
-    root_a = find(parent, a)
-    root_b = find(parent, b)
-
-    if root_a < root_b:
-        parent[root_b] = root_a
-    else:
-        parent[root_a] = root_b
-
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
+def main() -> None:
+    for _ in range(int(input())):
         n, m = map(int, input().split())
-        edges = [tuple(map(int, input().split())) for _ in range(m)]
-        parent = [i for i in range(n + 1)]
-        cnt = 0
+        airplane = [input() for _ in range(m)]
+        print(n - 1)
 
-        for a, b in edges:
-            if find(parent, a) != find(parent, b):
-                union(parent, a, b)
-                cnt += 1
 
-        print(cnt)
+main()
