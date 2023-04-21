7c76de00dea1d645e0e0f9ac47674009619bc058
Yiseull <omjl5123@catholic.ac.kr>
2023-04-21 19:42:36 +0900

    First solution: 가중 독립 집합 문제
    DAG의 최장 경로 문제로 변형

---
+++ 골드/19623_회의실 배정 4.py
@@ -0,0 +1,27 @@
+import sys
+from heapq import *
+input = sys.stdin.readline
+
+
+def solution() -> None:
+    n = int(input())
+    meeting = [tuple(map(int, input().split())) for _ in range(n)]
+    meeting.sort()
+    proceeding, finished = [], []
+
+    for start, end, people in meeting:
+        while proceeding and proceeding[0][0] <= start:
+            heappush(finished, -heappop(proceeding)[1])
+
+        if finished:
+            heappush(proceeding, (end, people + -finished[0]))
+        else:
+            heappush(proceeding, (end, people))
+
+    while proceeding:
+        heappush(finished, -heappop(proceeding)[1])
+
+    print(-finished[0])
+
+
+solution()
