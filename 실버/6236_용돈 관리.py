cb721e7dd5548345bb5b93607d98eec715cc2374
Yiseull <omjl5123@catholic.ac.kr>
2023-04-26 20:52:37 +0900

    :zap: Second solution: 이분 탐색

--- 실버/6236_용돈 관리.py
+++ 실버/6236_용돈 관리.py
@@ -3,31 +3,26 @@ input = sys.stdin.readline
 
 
 def solution() -> None:
-    n, m = map(int, input().split())
+    n, m = map(int, input().split() )
     balance = [int(input()) for _ in range(n)]
-    result = 1000000000
-    left, right = min(balance), 1000000000
+    left, right = max(balance), 1000000000
 
-    while left <= right:
+    while left < right:
         mid = (left + right) // 2
-        cur, cnt = 0, 0
+        cur, cnt = mid, 1
 
         for bal in balance:
             if cur < bal:
                 cnt += 1
                 cur = mid
             cur -= bal
-            if cur < 0 or cnt > m:
-                cnt = m + 1
-                break
 
         if cnt > m:
             left = mid + 1
         else:
-            result = min(result, mid)
-            right = mid - 1
+            right = mid
 
-    print(result)
+    print(left)
 
 
 solution()
