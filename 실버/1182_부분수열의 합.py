826afd392bbb8313653ffe13548decc0ff9fdb42
Yiseull <omjl5123@catholic.ac.kr>
2023-05-06 19:53:56 +0900

    :sparkles: Second solution: 중간에서 만나기

--- 실버/1182_부분수열의 합.py
+++ 실버/1182_부분수열의 합.py
@@ -1,14 +1,38 @@
-def dfs(idx, sum, cnt) -> None:
-    if sum == s and cnt > 0:
-        global answer
-        answer += 1
-
-    for i in range(idx, n):
-        dfs(i + 1, sum + sequence[i], cnt + 1)
+from bisect import bisect_left, bisect_right
+from itertools import combinations
 
 
-n, s = map(int, input().split())
-sequence = tuple(map(int, input().split()))
-answer = 0
-dfs(0, 0, 0)
-print(answer)
+def count(subset, target) -> int:
+    return bisect_right(subset, target) - bisect_left(subset, target)
+
+
+def calc_subset(set, subset, n) -> None:
+    for i in range(1, n + 1):
+        for comb in combinations(set, i):
+            subset.append(sum(comb))
+
+
+def solve() -> int:
+    x = []
+    y = []
+
+    calc_subset(seq[:n // 2], x, n // 2)
+    calc_subset(seq[n // 2:], y, n - n // 2)
+
+    x.sort()
+    y.sort()
+
+    cnt = 0
+    for i in x:
+        cnt += count(y, s - i)
+
+    cnt += count(x, s)
+    cnt += count(y, s)
+
+    return cnt
+
+
+if __name__ == '__main__':
+    n, s = map(int, input().split())
+    seq = list(map(int, input().split()))
+    print(solve())
