96c02be961716e73178eb4abbe41a9164efa03b7
Yiseull <omjl5123@catholic.ac.kr>
2023-05-10 17:54:44 +0900

    :zap: 실행 속도 단축 + Python 3 Recursion Error 해결

--- 골드/1103_게임.py
+++ 골드/1103_게임.py
@@ -1,25 +1,26 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
 
 
 def dfs(x, y) -> int:
-    if dp[x][y] != -1:
+    if visited[x][y]:
+        print(-1)
+        exit()
+
+    if dp[x][y]:
         return dp[x][y]
+
+    dp[x][y] = 1
 
     for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
         X = int(mapp[x][y])
         next_x = x + (dx * X)
         next_y = y + (dy * X)
         if 0 <= next_x < n and 0 <= next_y < m and mapp[next_x][next_y] != 'H':
-            if visited[next_x][next_y]:
-                print(-1)
-                exit()
-
-            visited[next_x][next_y] = True
+            visited[x][y] = True
             dp[x][y] = max(dp[x][y], dfs(next_x, next_y) + 1)
-            visited[next_x][next_y] = False
-        else:
-            dp[x][y] = max(dp[x][y], 1)
+            visited[x][y] = False
 
     return dp[x][y]
 
@@ -28,6 +29,5 @@ if __name__ == '__main__':
     n, m = map(int, input().split())
     mapp = [list(input().strip()) for _ in range(n)]
     visited = [[False] * m for _ in range(n)]
-    visited[0][0] = True
-    dp = [[-1] * m for _ in range(n)]
+    dp = [[0] * m for _ in range(n)]
     print(dfs(0, 0))
