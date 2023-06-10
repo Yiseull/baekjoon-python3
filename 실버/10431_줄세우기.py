09f6734bc95beb24397acccc71ea9e7e6bc0b1d5
Yiseull <omjl5123@catholic.ac.kr>
2023-06-10 12:06:16 +0900

    :zap: sorted_arr 리스트 재정의

--- 실버/10431_줄세우기.py
+++ 실버/10431_줄세우기.py
@@ -7,13 +7,12 @@ p = int(input())
 for t in range(1, p + 1):
     answer = 0
     arr = list(map(int, input().split()))[1:]
-    sorted_arr = [0] * 20
-    sorted_arr[0] = arr[0]
+    sorted_arr = [arr[0]]
     for i in range(1, 20):
         if sorted_arr[i - 1] > arr[i]:
             index = bisect_left(sorted_arr[:i], arr[i])
             sorted_arr.insert(index, arr[i])
             answer += i - index
         else:
-            sorted_arr[i] = arr[i]
+            sorted_arr.append(arr[i])
     print(t, answer)
